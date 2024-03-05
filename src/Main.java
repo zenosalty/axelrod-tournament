8e2427007a952217f3a04127bff2d4fbdd1b3820
zeno <zenosaletti@gmail.com>
2024-03-05 16:33:29 +0100

    changes to Main

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,17 @@ import strategies.prescient.Simpleton;
 
 public class Main {
     public static void main(String[] args) {
-        int rounds = 10;
+
+        int rounds = 10; //set number of rounds
 
         Player random = new Player(new Random());
         Player simpleton = new Player (new Simpleton());
 
-        Match m = new Match();
+        Match m = new Match(); // new match
 
-        m.setPlayers(simpleton, random);
-        m.fight(rounds);
+        m.setPlayers(simpleton, random); //set the players
+        m.fight(rounds); //and fight
 
-        System.out.println(simpleton.getScore() + " " + random.getScore());
+        System.out.println(simpleton.getScore() + " " + random.getScore()); //print scores
     }
 }
